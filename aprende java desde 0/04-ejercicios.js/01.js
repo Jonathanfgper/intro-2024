5d582f6aaa1e445748ac927958a8c3b9da83ff53
Jonathafg <Jonathanfgper@gmail.comgit config --global user.email Jonathanfgper@gmail.comgit config --global user.email Jonathanfgper@gmail.com>
2024-05-11 00:31:25 -0600

    arreglos

---
+++ aprende java desde 0/04-ejercicios.js/01.js
@@ -0,0 +1,12 @@
+function cualEsmayor(a, b){
+    // if (a > b) {
+    //     return a; 
+    // } else {
+    //     return b;
+    // }
+    return (a > b) ? a : b;
+
+}
+let mayor = cualEsmayor(10, 5);
+
+console.log(mayor);
---
+++ aprende java desde 0/04-ejercicios.js/02.js
@@ -0,0 +1,23 @@
+/**
+ * nombre: ancho x alto
+ * 8K 7680 X 4320
+ * 4K 3840 X 2160
+ * WQHD 2560 X 1140
+ * FHD 1920 X 1080
+ * HD 1280 X 720
+ */
+function nombreResolucion(ancho , alto) {
+    if (ancho > 7689 && alto >= 4320) {
+        return "8k";
+    }else if (ancho >= 3840 && alto >= 2160){
+        return "4K";
+    }else if (ancho >= 2560 && alto >= 1140){
+        return "WQHD";
+    }else if (ancho >= 1920 && alto >= 1080){
+        return "FHD";
+    }else if (ancho >= 1280 && alto >= 720){
+        return "HD";
+    }
+ }
+let nombre = nombreResolucion(1366, 768)
+console.log(nombre);
